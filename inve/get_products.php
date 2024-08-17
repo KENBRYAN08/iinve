8ce6bcc17f8bc738c515666de562ba058861bf56
KENBRYAN08 <145206661+KENBRYAN08@users.noreply.github.com>
2024-08-17 10:58:02 +0800

    Add files via upload

---
+++ inve/get_products.php
@@ -0,0 +1,16 @@
+<?php
+include 'config.php';
+
+$sql = "SELECT * FROM products";
+$result = $conn->query($sql);
+
+$products = array();
+while($row = $result->fetch_assoc()) {
+    $products[] = $row;
+}
+
+header('Content-Type: application/json');
+echo json_encode($products);
+
+$conn->close();
+?>
